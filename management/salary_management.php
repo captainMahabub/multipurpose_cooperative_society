13d4b3603cf8cd31f1425e9d09a03947f57a580b
CaptainMahabub <mahabuburrahman.bd92@gmail.com>
2016-08-02 02:56:56 +0600

    part 5 uploaded

---
+++ management/salary_management.php
@@ -0,0 +1,5 @@
+<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet">Employee Management</span>&nbsp; &nbsp;<span class="label label-important"> 2 </span></a>
+							<ul>
+								<li><a class="submenu" href="submenu.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> New Employee</span></a></li>
+								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> View Employee</span></a></li>
+							</ul>	
